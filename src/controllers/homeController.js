e2550d4014155db0d2e898471560805e4b7ad47f
JoaquinLGerez <joaquinlgerez@gmail.com>
2022-03-10 02:26:52 -0300

    Vistas de footer funcionales

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -5,7 +5,32 @@ let controller = {
 
 sobreNosotros: (req, res) => {
     res.render('sobreNosotros')
+},
+
+calidad: (req, res) => {
+    res.render('calidad')
+},
+exportacion: (req, res) => {
+    res.render('exportacion')
+},
+//Vista distribuidores no funciona en la pagina oficial lo cual no sabia que poner en esta vista, 
+///la deje armada pero falta rellenar con lo que va.
+distribuidores: (req, res) => {
+    res.render('distribuidores')
+},
+
+competicion: (req, res) => {
+    res.render('competicion')
+},
+
+servicio: (req, res) => {
+    res.render('servicio')
+},
+
+newsletters: (req, res) => {
+    res.render('newsletters')
 }
+
 
 }
 
--- src/routes/homeRouter.js
+++ src/routes/homeRouter.js
@@ -5,7 +5,29 @@ let controller = require('../controllers/homeController.js') // Requerimos el co
 
 // GET - Listado de productos
 router.get('/', controller.home) // buscamos en el objeto controller el HOME
+/* Links de nav - Empresa */
 router.get('/sobre-nosotros', controller.sobreNosotros) 
+router.get('/calidad', controller.calidad) 
+router.get('/exportacion', controller.exportacion) 
+router.get('/distribuidores', controller.distribuidores) 
+
+/* Links de nav - Competicion */
+router.get('/competicion', controller.competicion) 
+
+/* Links de nav - Productos */
+
+/* Links de nav - Catalogo */
+
+/* Links de nav - Servicio */
+router.get('/servicio', controller.servicio) 
+
+router.get('/newsletters', controller.newsletters) 
+
+/* Links de nav - Contacto */
+
+
+
+
 
 
 module.exports = router
