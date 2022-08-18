4963544ec5efe1b222c04e548e4db42f10443fee
rich-romero <rik.romero@gmail.com>
2022-08-18 12:20:43 -0300

    Ajustes Upload 1.031 Limites de bases/validadores2

--- src/middlewares/uploadImages.js
+++ src/middlewares/uploadImages.js
@@ -11,7 +11,7 @@ const storage = multer.diskStorage({
 })
 
 const fileFilter = function(req, file,callback) {
-    if(!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)){
+    if(!file.originalname.match(/\.(jpg|jpeg|png|webp|gif)$/)){
         req.fileValidationError = "Sólo imágenes (.jpg, .jpeg, .png, .gif)";
         return callback(null,false,req.fileValidationError);
     }
